bcdacdb57ee4c5c7a3097387c64cd444f7432287
JetStCover <151614082+JetStCover@users.noreply.github.com>
2024-01-28 22:20:13 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ public class Main {//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
     public static void main(String[] args) {
-        int startBalance = 2000;
-        int repAmount = 13548;
-        boolean Correct = (repAmount > 1000);
+        int balance = 0;
+        int amount = 1100;
+        boolean correct = (amount > 1000);
 
         int bonus;
-        int cent = (repAmount / 100);
-        if (Correct) {
+        int cent = (amount / 100);
+        if (correct) {
             bonus = cent;
         } else {
             bonus = 0;
         }
         
-        int sumBalance = startBalance + repAmount + bonus;
+        int sumBalance = balance + amount + bonus;
 
         System.out.println("Общий баланс: " + sumBalance);
     }
