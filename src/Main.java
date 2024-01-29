87ea7310bd88dfad2279eccd54eb5b78cd8d723a
Vladislav Klepikov <127222172+yalosyash@users.noreply.github.com>
2024-01-29 11:52:41 +0300

    Update Main.java
    уменьшил количество кода, увеличил скорость работы

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
-public class Main {//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+public class Main {
     public static void main(String[] args) {
         int balance = 0;
         int amount = 1100;
-        boolean correct = (amount > 1000);
-
-        int bonus;
-        int cent = (amount / 100);
-        if (correct) {
-            bonus = cent;
-        } else {
-            bonus = 0;
-        }
-        
+        int bonus = amount > 1000 ? amount / 100 : 0;
         int summary = balance + amount + bonus;
 
         System.out.println("Общий баланс: " + summary);
